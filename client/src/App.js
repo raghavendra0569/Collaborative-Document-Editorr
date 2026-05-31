6d9ed615403c0ebda92fcb78ab0afef47206039a
raghavendra0569 <raghavendra18052005@gmail.com>
2026-05-31 19:34:50 +0530

    Updated Tas 3 Collaborative Editor

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://collaborative-document-editorr.onrender.com");
 
 function App() {
   const [text, setText] = useState("");
